2dc392c76f6185770a5c9a78d784c95b237da837
yisakm9 <yisakm9@users.noreply.github.com>
2026-04-05 18:00:51 +0300

    Add deletion_policy=ABANDON to VPC peering for clean destroy

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -49,4 +49,5 @@ resource "google_service_networking_connection" "private_service" {
   network                 = google_compute_network.main.id
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.private_service_range.name]
+  deletion_policy         = "ABANDON"
 }
